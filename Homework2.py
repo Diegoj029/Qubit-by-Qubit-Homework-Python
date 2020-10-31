19317dd0ec81ca6fb0b35e9277fadce2390873d9
Diego Pérez <30667045+Diegoj029@users.noreply.github.com>
2020-10-31 01:54:02 -0600

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 31 01:32:43 2020
+
+@author: diego
+"""
+
+import numpy as np
+
+"""
+Convertidor coordenadas cartesianas a polares
+"""
+def convert_car(x,y):
+    r = np.sqrt(x ** 2 + y ** 2)
+    
+    if x != 0:
+        theta = np.arctan(y/x)
+    else:
+        theta = np.pi / 2
+        if y < 0:
+            theta *= 3
+    
+    return round(r,2), round(theta,2)
+
+#Ejemplo
+#print(convert_car(0, 1))
+
+"""
+Convertidor coordenadas polares a cartesianas
+"""
+def convert_pol(r,theta):
+    x = r * np.cos(theta)
+    y = r * np.sin(theta)
+
+    return round(x,2), round(y,2)
+
+#Ejemplo
+#print(convert_pol(2, (11 * np.pi) / 6))
